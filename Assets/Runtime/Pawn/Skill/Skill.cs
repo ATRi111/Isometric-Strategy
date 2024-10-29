24b8b2001069045ca2c9d1cd61a0c0a49a7c1888
1657125180@qq.com <1657125180@qq.com>
2024-10-29 15:14:14 +0800

    fix:完善场景加载框架

--- Assets/Runtime/Pawn/Skill/Skill.cs
+++ Assets/Runtime/Pawn/Skill/Skill.cs
@@ -5,11 +5,17 @@ public abstract class Skill : ScriptableObject
 {
     public int actionTime;
 
+    /// <summary>
+    /// 获取可选的技能施放位置
+    /// </summary>
     public virtual void GetOptions(Vector2Int position, List<Vector2Int> ret)
     {
         ret.Clear();
     }
 
+    /// <summary>
+    /// 模拟技能可能产生的效果
+    /// </summary>
     public virtual void Mock(PawnEntity agent, Vector2Int target,IsometricGridManager igm, EffectUnit ret)
     {
         ret.timeEffect.current += actionTime;
