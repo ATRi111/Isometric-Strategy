d3d9698c10ac7d44b84469178dd8d708842796ae
1657125180@qq.com <1657125180@qq.com>
2024-11-22 16:37:53 +0800

    feat:装备

---
+++ Assets/Runtime/Entity/Pawn/Equipment.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "新装备", menuName = "装备")]
+public class Equipment : PawnPropertyModifierSO
+{
+    public List<Skill> skillsAttached;
+
+    public override void Register(PawnEntity pawn)
+    {
+        base.Register(pawn);
+        for (int i = 0; i < skillsAttached.Count; i++)
+        {
+            pawn.Brain.Learn(skillsAttached[i]);
+        }
+    }
+
+    public override void Unregister(PawnEntity pawn)
+    {
+        base.Unregister(pawn);
+        for (int i = 0; i < skillsAttached.Count; i++)
+        {
+            pawn.Brain.Forget(skillsAttached[i]);
+        }
+    }
+}
