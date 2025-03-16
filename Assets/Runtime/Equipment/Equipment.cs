9610d2df162308a1a0218f606f36b70e689290fe
1657125180@qq.com <1657125180@qq.com>
2025-03-16 18:08:53 +0800

    chore:战斗开始时参数描述

--- Assets/Runtime/Equipment/Equipment.cs
+++ Assets/Runtime/Equipment/Equipment.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "新装备", menuName = "装备")]
@@ -17,5 +18,22 @@ public class Equipment : PawnModifierSO
             PawnParameterModifier modifier = parameterOnAgent[i];
             pawn.parameterDict[modifier.ParameterName] += modifier.deltaValue;
         }
+    }
+
+    protected override void Describe(StringBuilder sb)
+    {
+        base.Describe(sb);
+        if(parameterOnAgent.Count > 0)
+            DescribeParameter(sb);
+    }
+
+    protected virtual void DescribeParameter(StringBuilder sb)
+    {
+        sb.AppendLine("战斗开始时:");
+        for (int i = 0;i < parameterOnAgent.Count;i++)
+        {
+            parameterOnAgent[i].Describe(sb, "自身");
+        }
+        sb.AppendLine();
     }
 }
