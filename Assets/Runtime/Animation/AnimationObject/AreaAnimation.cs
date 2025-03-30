765c26448bead95e535f17e69cc889912210e97c
1657125180@qq.com <1657125180@qq.com>
2025-03-30 13:06:35 +0800

    feat:有向技能特效框架

--- Assets/Runtime/Animation/AnimationObject/AreaAnimation.cs
+++ Assets/Runtime/Animation/AnimationObject/AreaAnimation.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 在技能作用范围内各位置生成同样物体的动画
+/// </summary>
 public class AreaAnimation : AnimationObject
 {
     public string prefabName;
---
+++ Assets/Runtime/Animation/AnimationObject/DirectionalAnimation.cs
@@ -0,0 +1,24 @@
+using MyTool;
+using UnityEngine;
+
+/// <summary>
+/// 在技能释放位置生成一个物体并根据技能方向旋转的动画
+/// </summary>
+public class DirectionalAnimation : AnimationObject
+{
+    public string prefabName;
+
+    public override void Initialize(IAnimationSource source)
+    {
+        base.Initialize(source);
+        if(!string.IsNullOrWhiteSpace(prefabName))
+        {
+            PawnAction action = source as PawnAction;
+            Vector3Int cellDirection = action.target - action.position;
+            Vector2 direction = igm.CellToWorld(cellDirection);
+            float angle = direction.ToAngle();
+            Vector3 worldPosition = igm.CellToWorld(action.target);
+            objectManager.Activate(prefabName, worldPosition, new Vector3(angle, 0, 0), transform);
+        }
+    }
+}
