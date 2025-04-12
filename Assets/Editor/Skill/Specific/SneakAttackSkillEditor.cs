f95d23334c2e2dcab13d760f21d0099a915d92ab
1657125180@qq.com <1657125180@qq.com>
2025-04-12 18:04:06 +0800

    feat:背刺技能无视护甲

--- Assets/Editor/Skill/Specific/SneakAttackSkillEditor.cs
+++ Assets/Editor/Skill/Specific/SneakAttackSkillEditor.cs
@@ -5,11 +5,12 @@ using UnityEditor;
 public class SneakAttackSkillEditor : RangedSkillEditor
 {
     [AutoProperty]
-    public SerializedProperty damageAmplifier;
+    public SerializedProperty damageAmplifier, ignorePierceResistance;
 
     protected override void MyOnInspectorGUI()
     {
         base.MyOnInspectorGUI();
         damageAmplifier.FloatField("背刺伤害增幅");
+        ignorePierceResistance.IntField("无视护甲");
     }
 }
--- Assets/Runtime/Skill/Specific/SneakAttackSkill.cs
+++ Assets/Runtime/Skill/Specific/SneakAttackSkill.cs
@@ -15,18 +15,27 @@ public class SneakAttackSkill : RangedSkill
     }
 
     public float damageAmplifier;
+    public int ignorePierceResistance;
 
     protected override float MockDamageAmplifier(IsometricGridManager igm, PawnEntity agent, Entity victim, Vector3Int position, Vector3Int target)
     {
+        float k = 1f;
         if (SneakCheck(agent, victim))
-            return damageAmplifier;
-        return 0f;
+            k += damageAmplifier;
+        int pierceResistance = victim.DefenceComponent.resistance[EDamageType.Pierce].IntValue;
+        k *= pierceResistance / (pierceResistance - ignorePierceResistance);
+        return k - 1f;
     }
 
     protected override void Describe(StringBuilder sb)
     {
         base.Describe(sb);
+        sb.Append("无视目标");
+        sb.Append(ignorePierceResistance);
+        sb.Append("点穿刺抗性");
+        sb.AppendLine();
         sb.Append("从背后攻击敌人时,伤害提高");
         sb.Append(damageAmplifier.ToString("P0"));
+        sb.AppendLine();
     }
 }
