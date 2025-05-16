187efda944b1f2330904e74bee2f802c124ac78d
1657125180@qq.com <1657125180@qq.com>
2025-05-16 10:14:55 +0800

    chore:战斗日志初始时隐藏

--- Assets/Runtime/UI/BattleLog.cs
+++ Assets/Runtime/UI/BattleLog.cs
@@ -1,7 +1,8 @@
-using Services;
+﻿using Services;
 using Services.Event;
 using System.Collections;
 using TMPro;
+using UIExtend;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,6 +12,7 @@ public class BattleLog : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI tmp;
     private ScrollRect scrollRect;
+    private CanvasGroupPlus canvasGroup;
 
     public void Log(string message)
     {
@@ -30,24 +32,35 @@ public class BattleLog : MonoBehaviour
         scrollRect.normalizedPosition = Vector2.zero;
     }
 
+    private void BeforeBattle()
+    {
+        canvasGroup.Visible = true;
+    }
+
     private void AfterBattle(bool _)
-        => Clear();
+    {
+        canvasGroup.Visible = false;
+        Clear();
+    }
 
     private void Awake()
     {
         eventSystem = ServiceLocator.Get<IEventSystem>();
         scrollRect = GetComponentInChildren<ScrollRect>();
+        canvasGroup = GetComponent<CanvasGroupPlus>();
     }
 
     private void OnEnable()
     {
         eventSystem.AddListener<string>(EEvent.BattleLog, Log);
         eventSystem.AddListener<bool>(EEvent.AfterBattle, AfterBattle);
+        eventSystem.AddListener(EEvent.BeforeBattle, BeforeBattle);
     }
 
     private void OnDisable()
     {
         eventSystem.RemoveListener<string>(EEvent.BattleLog, Log);
         eventSystem.RemoveListener<bool>(EEvent.AfterBattle, AfterBattle);
+        eventSystem.RemoveListener(EEvent.BeforeBattle, BeforeBattle);
     }
 }
